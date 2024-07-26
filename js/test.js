80d271998577b695aafd79a43c540b4855443d42
searis7099 <clip.searis86@gmail.com>
2024-07-26 18:44:08 +0900

    initial commit

--- js/test.js
+++ js/test.js
@@ -33,3 +33,69 @@ $(function () {
 	  autoplaySpeed: 2800,
 	});
   });
+
+
+
+
+//const p = document.getElementById("myid");
+//console.log(p);
+
+
+const div = document.getElementById("wrap");
+div.innerHTML = '<p><strong>こ</strong>んにちは！</p>';
+//基本要素 書き換え
+
+const h1 = document.createElement('h1');
+      
+h1.textContent = 'こんにちは';
+
+document.body.appendChild(h1);
+
+console.log(h1);
+
+let fruits = "apple";
+console.log(fruits);
+
+
+const buttonadd = document.getElementById('button-add');
+const buttonClear = document.getElementById('button-clear');
+const list = document.getElementById('list');
+
+buttonadd.addEventListener('click', function(){
+	const element = document.createElement('li');
+	element.innerHTML = '中身の要素';
+	list.appendChild(element);// listにelementを追加する
+});
+
+buttonClear.addEventListener('click', function(){
+	list.removeChild(list.lastChild);
+});
+
+
+const twitter = document.getElementById('twitter');
+twitter.innerHTML = '<img src="img/twitter.png" class="smalllogo"><a href="https://x.com" class="twitter" id="none">twitter</a> <br>';
+//const username = document.getElementById('none');
+//username.innerHTML = ''
+//function Replace(){
+	let link = document.getElementById('none');
+	let url = "https://x.com/kiri__tsuki";
+
+	link.setAttribute('href', url);
+
+var sampleElement = document.getElementById("twitter2");
+	result = sampleElement.classList.contains(twitter2);
+	
+console.log(result);
+
+
+var p = $("p").hasclass("twitter2");
+ 
+if( p ) {
+ 
+	console.log('検出されました！');
+	
+  } else {
+	
+	console.log('検出できませんでした！');
+	
+  }
